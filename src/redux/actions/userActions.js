7f1e1c6a21e76b68d09d3854be8b070bbc38a49b
fratellomio <mr.gomu@gmail.com>
2020-07-31 11:23:58 +0700

    change stuff

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -22,7 +22,9 @@ export const loginUser = (userData, history) => (dispatch) => {
     .catch((err) => {
       dispatch({
         type: SET_ERRORS,
-        payload: err.response.data,
+        payload: err.response.data.errors
+          ? err.response.data.errors
+          : err.response.data,
       });
     });
 };
